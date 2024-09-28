0d6463cb9d1aaac1210f0e16ddecea49b15a3707
briannoelk <e0958132@u.nus.edu>
2024-09-28 06:07:16 +0200

    format

--- frontend/src/🏠_Home.py
+++ frontend/src/🏠_Home.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Home Page",
+    page_title="AIRA",
     page_icon="📄",
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+st.sidebar.title("AIRA")
+st.sidebar.markdown("<h5>Your Intelligent Research Assistant</h5>", unsafe_allow_html=True)  # Custom HTML for smaller title
 
 st.title("Welcome to the Streamlit App")
